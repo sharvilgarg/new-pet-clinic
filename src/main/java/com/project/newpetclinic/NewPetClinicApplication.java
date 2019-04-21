9ccfabfb03543726019b5f134071fe4f4e783203
sharvilgarg <sharvil.garg@gmail.com>
2019-04-22 02:17:39 +0530

    add POJOs

---
+++ src/main/java/com/project/newpetclinic/NewPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.project.newpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/project/newpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.project.newpetclinic.model;
+
+public class Owner extends Person {
+}
