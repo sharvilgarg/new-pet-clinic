5bdb598a7f0ab93969c77d25414f4bb51f3a02de
sharvilgarg <sharvil.garg@gmail.com>
2019-04-22 16:58:18 +0530

    multi module maven project

--- pet-clinic-web/src/test/java/com/project/newpetclinic/NewPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/com/project/newpetclinic/NewPetClinicApplicationTests.java
@@ -1,12 +1,12 @@
-package newpetclinic;
+package com.project.newpetclinic;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@ContextConfiguration
+@SpringBootTest
 public class NewPetClinicApplicationTests {
 
     @Test
