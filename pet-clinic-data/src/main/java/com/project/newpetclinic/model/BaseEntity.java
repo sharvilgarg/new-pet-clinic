ec6c977fa9967662e8888af2a4128c739884acb1
sharvilgarg <sharvil.garg@gmail.com>
2019-06-07 16:34:21 +0530

    Web Development with Spring MVC:Done

--- pet-clinic-data/src/main/java/com/project/newpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/project/newpetclinic/model/BaseEntity.java
@@ -1,9 +1,17 @@
 package com.project.newpetclinic.model;
 
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
@@ -13,5 +21,4 @@ public class BaseEntity implements Serializable {
     public void setId(Long id) {
         this.id = id;
     }
-    public Long setter;
 }
